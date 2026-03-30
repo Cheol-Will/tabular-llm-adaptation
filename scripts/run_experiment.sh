9d31740dde8b3a7a560c5d523f90e3afcf934526
Cheol-Will <kangcs7083@naver.com>
2026-03-30 07:29:52 +0000

    temp

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,17 +1,20 @@
 # model="FTTransformer"
-# model="TFMLLM"
+# model="LLMBaseline"
+model="TFMLLM"
 
-model="LLMBaseline"
 # model_cls_name="LLMBaseline"
 # model_cls_name="LLMBaselineBidirectional"
 # model_cls_name="LLMBaselinePooling"
-model_cls_name="LLMBaselineBidirectionalPooling"
-exp_name="260326"
+# model_cls_name="LLMBaselineBidirectionalPooling"
+# exp_name="260326"
+exp_name="260320-num_emb"
 num_random_configs=10 # for fast experiment
 
 python experiment/main.py \
     --model $model \
     --exp_name $exp_name \
-    --model_cls_name $model_cls_name \
-    --subset "small-large-features"
-    # --subset "small"
+    --subset "small" \
+    --num_random_configs $num_random_configs
+
+    # --subset "small-large-features"
+    # --model_cls_name $model_cls_name \
