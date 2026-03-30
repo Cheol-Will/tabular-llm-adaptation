d6790022cd079857d92fcf3bc78b6605447bbc25
Cheol-Will <kangcs@naver.com>
2026-03-30 07:31:14 +0000

    temp

--- scripts/run_evaluate.sh
+++ scripts/run_evaluate.sh
@@ -1,5 +1,22 @@
-# model="LLMBaseline"
-model="FTTransformer"
+
+# generate cache
+# exp_name="260323"
+# new_model="FTTransformer"
+
+# exp_name="260320-num_emb"
+# new_model="TFMLLM"
+
+# exp_name="260326"
+# new_model="LLMBaselineBidirectional"
+
+# python experiment/evaluate.py \
+#     --model $new_model\
+#     --exp_name $exp_name \
+#     --generate_cache
+
+
+# summary
+model="LLMBaseline"
 python experiment/evaluate.py \
-    --model $model\
+    --model $model \
     --exp_name "260323"
--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -5,13 +5,15 @@ model="LLMBaseline"
 # model_cls_name="LLMBaseline"
 # model_cls_name="LLMBaselineBidirectional"
 # model_cls_name="LLMBaselinePooling"
-model_cls_name="LLMBaselineBidirectionalPooling"
-exp_name="260326"
-num_random_configs=10 # for fast experiment
+# model_cls_name="LLMBaselineBidirectionalPooling"
+
+model="FTTransformer"
+exp_name="260323"
+num_random_configs=200 # for fast experiment
 
 python experiment/main.py \
     --model $model \
     --exp_name $exp_name \
-    --model_cls_name $model_cls_name \
-    --subset "small-large-features"
-    # --subset "small"
+    --subset "small"
+    # --model_cls_name $model_cls_name \
+    # --subset "small-large-features"
